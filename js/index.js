2053ef70b1879d226e7185f2e356cb1491108b3f
Hugo Meireles <meireles.hmm@gmail.com>
2018-12-24 01:26:12 +0000

    create layout and javascript configs

---
+++ js/index.js
@@ -0,0 +1,53 @@
+
+$(document).ready(() => {
+
+    let computerScore = 0;
+    let userScore = 0;
+    const optionToChoice = ['Rock', 'Paper', 'Scissor'];
+
+    let userChoice;
+    let computerChoice;
+
+
+    function wirteScores(userScore, computerScore){
+        $('#userScore').text(userScore);
+        $('#computerScore').text(computerScore);
+    }
+
+    function clearScores(){
+        $('#userScore').text('');
+        $('#computerScore').text('');
+    }
+
+    function writeTheRound(text){
+        let elem = `<p>${text}</p>`
+        $('.roundsHistorys').append(elem);
+    }
+
+
+    function game(){
+        setup();
+
+        let gameEnd = false;
+
+        while(gameEnd){
+
+            
+
+
+            clearScores(cScore);
+            wirteScores(uScore);
+
+            let output = `Player choice[${handPlayer}] and Computer choice[${handComputer}],  ${winner} win this round.`;
+            writeTheRound(output);
+        }
+
+
+    }    
+
+
+
+
+    
+
+});
